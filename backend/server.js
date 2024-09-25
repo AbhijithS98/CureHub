0b9329083197e93c6a2f2653ac8ff858498d8c84
AbhijithS98 <abhijithsb745@gmail.com>
2024-09-25 18:20:12 +0530

    Initial Project Setup

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import connectDB from './config/db.js'
+import dotenv from 'dotenv'
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+const app = express()
+connectDB()
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Server Ready'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-xl font-bold text-blue-500'>Hello TAILWIND CSS with React Vite</h1>
+    </>
+  )
+}
+
+export default App
