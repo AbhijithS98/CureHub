be62bf221b5d911265df0a936a5c4a0a855e09f4
AbhijithS98 <abhijithsb745@gmail.com>
2024-09-26 00:25:44 +0530

    restored stashed changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { urlencoded } from 'express'
 import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 const PORT = process.env.PORT || 5000;
@@ -11,3 +11,5 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('Server Ready'));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.use(express.urlencoded())
