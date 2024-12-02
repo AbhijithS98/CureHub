0fed2207967e2aae5e98ac164f912a5ca1d002ac
AbhijithS98 <abhijithsb745@gmail.com>
2024-12-02 23:47:41 +0530

    chat implementation started

---
+++ backend/models/chatSchema.ts
@@ -0,0 +1,23 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IChat extends Document {
+  doctorId: mongoose.Types.ObjectId; 
+  patientId: mongoose.Types.ObjectId; 
+  message: string;
+  isDoctorSender: boolean;
+  createdAt: Date; 
+}
+
+const chatSchema: Schema<IChat> = new Schema(
+  {
+    doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor", required: true },
+    patientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    message: { type: String, required: true },
+    isDoctorSender: { type: Boolean, required: true },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true } 
+);
+
+const Chat = mongoose.model<IChat>("Chat", chatSchema);
+export default Chat;
