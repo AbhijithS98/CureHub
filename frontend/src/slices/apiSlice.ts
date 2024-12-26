834a511bfd3be784bd16bf973b6a9e6f94cfb730
AbhijithS98 <abhijithsb745@gmail.com>
2024-12-26 18:42:04 +0530

    moved backend url to env

--- frontend/src/slices/apiSlice.ts
+++ frontend/src/slices/apiSlice.ts
@@ -7,9 +7,12 @@ import { setAdminToken, clearAdminCredentials } from "./adminSlices/adminAuthSli
 import { toast } from "react-toastify";
 const mutex = new Mutex();
 
+const apiBaseURL = process.env.REACT_APP_BACKEND_URL;
+console.log("backend url is: ",apiBaseURL);
+
 
 let baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: `${apiBaseURL}/api`,
   credentials: "include",
   prepareHeaders: (headers, { getState, endpoint }) => {
     let token;
